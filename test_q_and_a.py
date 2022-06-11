298fa2ace96905ec0d5a58b226227e18addaf632
Tigran Harutyunyan <harutyunyan.tigran.v@gmail.com>
2022-06-11 03:33:12 -0400

    Update test_q_and_a.py
    Fixing test file

--- test_q_and_a.py
+++ test_q_and_a.py
@@ -1,6 +1,11 @@
+import pytest
 from q_and_a import tester
 
+# Arrange
+@pytest.fixture
+def input_qn():
+    return "Who am I?, I am Robert"
 
-def test_tester():
-    test_string = "Who am I?, I am Robert"
-    assert isinstance(tester(test_string), str) == True
+
+def test_tester(input_qn):
+    assert isinstance(tester(input_qn), str) == True
